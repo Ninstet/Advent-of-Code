71fbe95ed3a3fa1995ae4d3bc6767688b75f2df3
Ninstet <jxw1025@student.bham.ac.uk>
2023-02-08 22:19:28 +0000

    Commented out days 7 and 8 main calls

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,7 @@ mod day_8;
 mod day_9;
 
 pub fn main() {
+    // day_7::main();
+    // day_8::main();
     day_9::main();
 }
--- target/debug/deps/advent_of_code_2022-c40403a25fbd08b8.d
+++ target/debug/deps/advent_of_code_2022-c40403a25fbd08b8.d
@@ -1,6 +1,8 @@
-/Users/ninstet/Documents/Advent-of-Code/target/debug/deps/advent_of_code_2022-c40403a25fbd08b8: src/main.rs src/day_9/mod.rs
+/Users/ninstet/Documents/Advent-of-Code/target/debug/deps/advent_of_code_2022-c40403a25fbd08b8: src/main.rs src/day_7/mod.rs src/day_8/mod.rs src/day_9/mod.rs
 
-/Users/ninstet/Documents/Advent-of-Code/target/debug/deps/advent_of_code_2022-c40403a25fbd08b8.d: src/main.rs src/day_9/mod.rs
+/Users/ninstet/Documents/Advent-of-Code/target/debug/deps/advent_of_code_2022-c40403a25fbd08b8.d: src/main.rs src/day_7/mod.rs src/day_8/mod.rs src/day_9/mod.rs
 
 src/main.rs:
+src/day_7/mod.rs:
+src/day_8/mod.rs:
 src/day_9/mod.rs:
